353e543d0f477923ac7fd8ccd53e7ddebb2d9c97
Piotr Mankowski <pwm4@pwm4-OptiPlex-990.(none)>
2012-06-15 13:33:07 -0400

    Initializing Project

--- README.md
+++
@@ -1,4 +0,0 @@
-Rely
-====
-
-Reliability Exercise Managment
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rely::Application.config.session_store :cookie_store, key: '_rely_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rely::Application.config.session_store :active_record_store
